c05aa229f7597852e8bea5c38160e9b4f9662aaa
lp206805 <59627088+lp206805@users.noreply.github.com>
2020-01-14 10:35:29 +0100

    c
    cf

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@
 
 <body>
     <?php
-    $retour = mail('leopiazza21@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'depuis : leopiazza21@gmail.com');
+    $retour = mail('leopiazza21@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From : lp206805.github.io');
     if ($retour) {
         echo '<p>Votre message a bien été envoyé.</p>';
     }
